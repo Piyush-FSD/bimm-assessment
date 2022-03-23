8a4b53f5063cbcce41ab9b5879c5ea55ef5222f1
Piyush Patel <piyushpatel.cloud@yahoo.com>
2022-03-23 17:27:45 -0400

    adding item in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
+import React, { useState } from 'react'
+
 function App() {
+  const [item, setItem] = useState({ items: [], text: "" })
+
+  const handleTextChange = (e) => {
+    setItem({ text: e.target.value })
+  }
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: Date.now(),
+      text: item.text,
+      done: false
+    }
+
+    setItem((prevState) => ({
+      items: prevState.items.concat(newItem),
+      text: ""
+    }))
+
+    // markItemCompleted(itemId) {
+    //   const updatedItems = item.map((elem) => {
+    //     if (itemId === item.id) {
+    //       item.done = !item.done
+    //     }
+    //     return elem;
+    //   })
+    // }
+  }
+
   return (
-    <div className="App">Hello World</div>
+    <div className="App">
+      <h1>To Do List</h1>
+      <div>
+        <input
+          type="text"
+          name="item"
+          onChange={handleTextChange}
+        />
+        <button onChange={handleAddItem}>Add</button>
+      </div>
+      {/* <div>{item.text}</div> */}
+    </div>
+
   );
 }
 
