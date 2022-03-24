d8c51ae69e28f1f8a3a40fa8ba6729f8e3cb03f9
Piyush Patel <piyushpatel.cloud@yahoo.com>
2022-03-24 17:02:08 -0400

    styling complete

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -46,55 +46,73 @@ export const ToDoApp = () => {
     };
 
     return (
-        <Container>
-            <h1>To Do List</h1>
-            <form onSubmit={handleAddItem}>
-                <FormContainer>
-                    <div>
-                        <Input
-                            type="text"
-                            value={item.text}
-                            onChange={handleTextChange}>
-                        </Input>
-                    </div>
-                    <div>
-                        <AddButton disabled={!item.text}>
-                            Add# {item.items.length + 1}
-                        </AddButton>
-                    </div>
-                </FormContainer>
-            </form>
-            <div>
-                <TodoList
-                    items={item.items}
-                    onItemCompleted={markItemCompleted}
-                    onDeleteItem={handleDeleteItem}
-                />
-            </div>
-        </Container>
+        <Containerwrapper>
+            <Container>
+                <h1>To Do List</h1>
+                <form onSubmit={handleAddItem}>
+                    <FormContainer>
+                        <div>
+                            <Input
+                                type="text"
+                                value={item.text}
+                                onChange={handleTextChange}>
+                            </Input>
+                        </div>
+                        <div>
+                            <AddButton disabled={!item.text}>
+                                Add #{item.items.length + 1}
+                            </AddButton>
+                        </div>
+                    </FormContainer>
+                </form>
+                <div>
+                    <TodoList
+                        items={item.items}
+                        onItemCompleted={markItemCompleted}
+                        onDeleteItem={handleDeleteItem}
+                    />
+                </div>
+            </Container>
+        </Containerwrapper>
     )
 };
 
+const Containerwrapper = styled.div`
+text-align: center;
+height: 100vh;
+`
+
 const Container = styled.div`
+border-radius: 60px;
 width: 500px;
-height: 300px;
+height: unset;
 border: 3px solid lightblue;
 text-align: center;
+margin: auto 0;
+display: inline-block;
+box-shadow: 8px 6px #bddce8;
+font-size: 20px;
 `
 
 const FormContainer = styled.div`
 display: flex;
-justify-content: center;
+justify-content: space-evenly;
 `
 
 const Input = styled.input`
-height: 40px;
+height: 45px;
 border-radius: 15px;
+width: 280px;
+border: 3px solid lightblue;
+font-size: 20px;
 `
 
 const AddButton = styled.button`
-color: green;
-border-radius: 15px;
-height: 40px;
-width: 80px;
+color: #222;
+background: lightblue;
+border-radius: 20px;
+height: 45px;
+width: 100px;
+box-shadow: 1px 1px 3px 1px #8cb2ee;
+font-size: 17px;
 `
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,17 @@
-import React, { useRef, useEffect } from 'react'
-
+import React, { useRef, useState, useEffect } from 'react'
+import styled from "styled-components";
 
 export const TodoItem = ({ id, text, completed, onItemCompleted, onDeleteItem, ch }) => {
     const ref = useRef();
+    const [cross, setCross] = useState(false)
+
+    const crossItem = () => {
+        setCross(!cross)
+    }
+
+    const crossStyling = {
+        textDecoration: cross ? "line-through" : "none",
+    };
 
     const markCompleted = () => {
         onItemCompleted(id)
@@ -25,18 +34,46 @@ export const TodoItem = ({ id, text, completed, onItemCompleted, onDeleteItem, c
     return (
         <div>
             {/* {completed ? "done" : "undone"} */}
-            <li>
+            <ListItem onClick={crossItem}>
                 {/*li ref={(li) => (listItem = li)}*/}
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={ch}
-                        onChange={markCompleted}
-                    />
-                    {text}
-                </label>
-                <button type="button" onClick={deleteItem}>x</button>
-            </li>
+                <LabelInputContainer>
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={ch}
+                            onChange={markCompleted}
+                        />
+                    </div>
+                    <ItemText style={crossStyling}>
+                        {text}
+                    </ItemText>
+                </LabelInputContainer>
+                <DeleteButton type="button" onClick={deleteItem}>x</DeleteButton>
+            </ListItem>
         </div>
     )
 }
+
+const ListItem = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 2vh;
+margin: 20px 30px 20px 0;
+background: rgb(174 219 255 / 63%);
+border: 1px solid #333;
+border-radius: 20px;
+`;
+
+const DeleteButton = styled.button`
+background: lightblue; 
+border-radius: 10px;
+width: 35px;
+height: 30px;
+`;
+const LabelInputContainer = styled.div`
+display: flex;
+`;
+
+const ItemText = styled.div`
+margin-left: 10px;
+`;
